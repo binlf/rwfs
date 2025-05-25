4f3f0643d8943319fcb559ff51ae82a904572226
binlf <killthelast01@gmail.com>
2025-05-25 21:23:58 +0100

    chore: migrate to 'tsdown' for bundling

--- bunup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "bunup";
-import { exports } from "bunup/plugins";
-
-export default defineConfig({
-  entry: "src/index.ts",
-  format: ["esm", "cjs"],
-  dts: true,
-  plugins: [exports()],
-});
